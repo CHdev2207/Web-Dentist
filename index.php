0c8be3a90ff9320530c28e474fa4dc9ea52e8eb0
CHdev2207 <145411718+CHdev2207@users.noreply.github.com>
2024-05-02 15:27:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,55 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Website Design Dentist</title>
+
+     <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+    <!-- bootstrap cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+    
 </head>
 <body>
+
+    <!-- header section starts  -->
+<header class="header fixed-top">
+
+    <div class="container">
+        <div class="navbar">
+
+            <a href="#home" class="logo">dental<span>Care.</span></a>
+
+            <nav class="nav">
+                <a href="#home">home</a>
+                <a href="#about">about</a>
+                <a href="#services">services</a>
+                <a href="#reviews">reviews</a>
+                <a href="#contact">contact</a>
+            </nav>
+
+            <a href="#contact" class="link-btn">make appointment</a>
+
+        </div>
+    </div>
+
+</header>
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
     
 </body>
 </html>
